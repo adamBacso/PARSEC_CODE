8d0b0d8334aff14bdcfeb225f8da98071272c7d8
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-12-10 13:02:21 +0100

    Create class MissionTime

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -1,7 +1,54 @@
-void setup(){
+#include <Wire.h>
+#include <SD.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
+// SERIAL COMMS
+#define USB_SERIAL              Serial      // serial through USB
+#define COMMS_SERIAL            Serial1     // serial through pin(1,0) [TX,RX]
+#define GPS_SERIAL              Serial2     // serial through pin(8,7) [TX,RX]
+
+// BME280
+Adafruit_BME280 bme;
+const double SEALEVELPRESSURE_HPA = (1013.25); // FIXME: replace by value true locally
+
+
+void setup(){
+    USB_SERIAL.begin(9600);
+    MissionTime timer;
+    
 }
 
 void loop(){
-    
+
 }
+
+
+
+class MissionTime{
+    private:
+        unsigned long startTime;
+        static unsigned int seconds, minutes, hours;
+    public:
+        MissionTime(){
+            reset();
+        }
+
+        void reset(){
+            startTime = millis();
+            seconds=0, minutes=0, hours=0;
+        }
+
+        String get_time(){
+            unsigned long elapsedTime = (millis()-startTime)/1000; // in seconds
+
+            seconds = elapsedTime % 60;
+            minutes = (elapsedTime % 3600) / 60;
+            hours = elapsedTime / 3600;
+
+            char buffer[9];
+            snprintf(buffer, sizeof(buffer), "%02u:%02u:%02u", hours, minutes, seconds);
+
+            return String(buffer);
+        }
+};
