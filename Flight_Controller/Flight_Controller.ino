e44f6a3abab4c53068d74fa16641daabd4e2281d
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-12-11 14:50:47 +0100

    reorganize program to adhere to laws of compilaiton add data logging functionality

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -1,68 +1,27 @@
 #include <Wire.h>
 #include <SD.h>
+#include <SPI.h>
+#include <FastCRC.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+#include <core_pins.h>
+#include <usb_serial.h>
+
 // SERIAL COMMS
 // serial through USB
-#define MY_USB_SERIAL              Serial      
+#define MY_USB_SERIAL               Serial      
 // serial through pin(1,0) [TX,RX]
-#define COMMS_SERIAL            Serial1     
+#define COMMS_SERIAL                Serial1     
 // serial through pin(8,7) [TX,RX]
-#define GPS_SERIAL              Serial2     
+#define GPS_SERIAL                  Serial2     
 
-// BME280
-// https://randomnerdtutorials.com/bme280-sensor-arduino-pressure-temperature-humidity/
-Adafruit_BME280 bme;
-const double SEA_LEVEL_PRESSURE_HPA = (1013.25); // FIXME: replace by value true locally
-
-
-void setup(){
-    MY_USB_SERIAL.begin(9600);
-}
-
-void loop(){
-
-}
-
-class Telemetry{
-    /*ID,MISSION_TIME,PACKET_COUNT,TEMPERATURE,BAROMETRIC_ALTITUDE,HUMIDITY,GPS_TIME,GPS_ALTITUDE,GPS_LONGITUDE,GPS_LATITUDE,TILT_X,TILT_Y,TILTZ,ACCELERATION_X,ACCELERATION_Y,ACCELERATION_Z,O3_CONCENTRATION,VOLTAGE,CHECKSUM*/
-    private:
-        const String ID = "12";
-        uint32_t packetCount;
-        MissionTime timer;
-    public:
-        String createPacket(){
-            String packet = ID + "," +
-                            String(packetCount) + "," +
-                            timer.get_time() + "," +
-                            String(bme.readTemperature()) + "," +
-                            String(bme.readAltitude(SEA_LEVEL_PRESSURE_HPA)) + "," +
-                            String(bme.readHumidity()) + "," // TODO: add readings from all components
-                            ;
-
-
-            return packet + getChecksum(packet);
-        }
-
-        void telemetryInit(){
-            packetCount = 0;
-            timer.reset();
-        }
-
-        String getChecksum(String data){
-            byte checksum = 0;
-            for (char ch : data){
-                checksum ^= ch;
-            }
-            return String(checksum,HEX);
-        }
-};
+const int chipSelect = 10;
 
 class MissionTime{
     private:
         unsigned long startTime;
-        static unsigned int seconds, minutes, hours;
+        unsigned int seconds, minutes, hours;
     public:
         MissionTime(){
             reset();
@@ -86,3 +45,82 @@ class MissionTime{
             return String(buffer);
         }
 };
+
+class Telemetry{
+    /*ID,MISSION_TIME,PACKET_COUNT,TEMPERATURE,BAROMETRIC_ALTITUDE,HUMIDITY,GPS_TIME,GPS_ALTITUDE,GPS_LONGITUDE,GPS_LATITUDE,TILT_X,TILT_Y,TILTZ,ACCELERATION_X,ACCELERATION_Y,ACCELERATION_Z,O3_CONCENTRATION,VOLTAGE,CHECKSUM*/
+    private:
+        const String ID = "12";
+        uint32_t packetCount;
+        MissionTime timer;
+
+        // BME280
+        // https://randomnerdtutorials.com/bme280-sensor-arduino-pressure-temperature-humidity/
+        Adafruit_BME280 bme;
+        const double SEA_LEVEL_PRESSURE_HPA = (1013.25); // FIXME: replace by value true locally
+    public:
+        String parse(){
+            String packet = ID + "," +
+                            String(packetCount) + "," +
+                            timer.get_time() + "," +
+                            String(bme.readTemperature()) + "," +
+                            String(bme.readAltitude(SEA_LEVEL_PRESSURE_HPA)) + "," +
+                            String(bme.readHumidity()) + "," // TODO: add readings from all components
+                            ;
+
+
+            return packet + getChecksum(packet);
+        }
+
+        void init(){
+            packetCount = 0;
+            timer.reset();
+        }
+
+        String getChecksum(String data){ // TODO: convert to using FastCRC
+            byte checksum = 0;
+            for (char ch : data){
+                checksum ^= ch;
+            }
+            return String(checksum,HEX);
+        }
+
+        void send(){
+            if (MY_USB_SERIAL.availableForWrite()>0){
+                MY_USB_SERIAL.println(parse());
+            }
+            if (COMMS_SERIAL.availableForWrite()>0){
+                COMMS_SERIAL.println(parse());
+            }
+        }
+
+        void write(){
+            File logFile = SD.open("logFile.txt", FILE_WRITE);
+            if (logFile){
+                logFile.println(parse());
+            }
+            logFile.close();
+        }
+
+        void communicate(){
+            write();
+            send();
+        }
+};
+
+Telemetry data;
+
+void setup(){
+    MY_USB_SERIAL.begin(9600); // TODO: add component validation
+    if (!SD.begin(chipSelect)){
+        // TODO: indicate that no SD card
+        while (1){
+            // stop here
+        }
+    }
+    data.init();
+}
+
+void loop(){
+    data.communicate();
+    delay(1000);
+}
