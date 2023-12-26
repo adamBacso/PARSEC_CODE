cf3f82fd169f8d12384ae0d933a369805262c877
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-12-26 13:10:58 +0100

    add remaining sensors

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -1,11 +1,12 @@
-#include <CRC32.h>
 
 #include <LoRa.h>
 #include <Wire.h>
 #include <SD.h>
 #include <SPI.h>
+#include <CRC32.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_MPU6050.h>
 
 #include <core_pins.h>
 #include <usb_serial.h>
@@ -20,11 +21,23 @@
 
 
 
-// BME280
-// https://randomnerdtutorials.com/bme280-sensor-arduino-pressure-temperature-humidity/
-Adafruit_BME280 bme;
+
 // GLOBALS
 const int SD_CARD_CHIP_SELECT = 10;
+
+class LightSensor {
+    private:
+        int analogPin;
+        float operatingVoltage;
+        int resolution = 1024;
+    public:
+        LightSensor(int pin, float voltage) : analogPin(pin), operatingVoltage(voltage){}
+
+        float readIntensity(){
+            float sensorValue = analogRead(analogPin);
+            return sensorValue/resolution*operatingVoltage;
+        }
+};
 
 class Led {
     public:
@@ -43,8 +56,7 @@ Led led;
 
 class MissionTime{
     private:
-        unsigned long startTime;
-        unsigned int seconds, minutes, hours;
+        unsigned long missionStartTime;
 
         unsigned long processStart;
     public:
@@ -53,25 +65,30 @@ class MissionTime{
         }
 
         void reset(){
-            startTime = millis();
-            seconds=0, minutes=0, hours=0;
+            missionStartTime = millis();
             processStart = 0;
         }
-
-        uint32_t get_time(){
-            uint32_t elapsedTime = (millis()-startTime)/1000; // in seconds
+        
+        String get_timestamp(){
+            unsigned int seconds, minutes, hours;
+            uint32_t elapsedTime = (millis()-missionStartTime)/1000; // in seconds
 
             seconds = elapsedTime % 60;
             minutes = (elapsedTime % 3600) / 60;
             hours = elapsedTime / 3600;
+            
+            String timestamp = "";
+            timestamp += hours + ":";
+            timestamp += minutes + ":";
+            timestamp += seconds;
 
-            if (minutes<10){
-                hours*=10;
-            }
-            else if (seconds<10){
-                minutes*=10;
-            }
-            return hours*10000 + minutes*100 + seconds;
+            return timestamp;
+        }
+        
+
+        float get_time(){
+            uint32_t elapsedTime = (millis()-missionStartTime) / 100; // in deciseconds xD
+            return float(elapsedTime/10); // should return seconds to one decimal precision
         }
 
         void true_sleep(int milli){
@@ -95,7 +112,11 @@ class Telemetry{
         float percentActive = 0.1;
         int sleepAmount;
 
+        // **COMPONENTS**
         static const int COMPONENT_COUNT = 4;
+        Adafruit_BME280 bme;
+        Adafruit_MPU6050 mpu;
+        LightSensor guva = LightSensor(14,3.3);
 
         std::string printBuffer = "";
         uint32_t bitSize;
@@ -130,6 +151,12 @@ class Telemetry{
             bitSize += sizeof(data) + sizeof(separator);
         }
 
+        void prints(String data, char separator = ','){
+            COMMS_SERIAL.print(data);
+            COMMS_SERIAL.print(separator);
+            printBuffer += data.c_str() + separator;
+        }
+
     public:
     /*
         String parse(){
@@ -151,21 +178,11 @@ class Telemetry{
         }
 
         void broadcast(){
-            /*
-            uint32_t currentTime = millis();
-            uint32_t startTime = broadcastStartTime;
-            if (broadcasting && (startTime+sleepAmount<currentTime)){
-                COMMS_SERIAL.println(currentTime);COMMS_SERIAL.println(startTime);COMMS_SERIAL.println(sleepAmount);
-                broadcastStartTime = currentTime;
-                this->telemetry_send();
-                //set_sleep_amount();
-                led.flash();
-            }
-            */
-            if (broadcasting && (millis()-broadcastStartTime>=1000)){
+            uint32_t elapsedTime = millis()-broadcastStartTime;
+            if (broadcasting && (elapsedTime>=sleepAmount)){
                 broadcastStartTime = millis();
                 this->telemetry_send();
-                //set_sleep_amount();
+                set_sleep_amount(elapsedTime);
             }
 
         }
@@ -185,10 +202,33 @@ class Telemetry{
                 this->prints(bme.readTemperature());
                 this->prints(bme.readHumidity());
             }else{
-                this->prints(0);
-                this->prints(0);
-                this->prints(0);
+                for (int i = 0; i<3; i++){
+                    this->prints(0);
+                }
             }
+
+            // MPU-6050
+            if (mpu.begin()){
+                sensors_event_t a, g, temp;
+                mpu.getEvent(&a, &g, &temp);
+                // acceleration
+                this->prints(a.acceleration.x);
+                this->prints(a.acceleration.y);
+                this->prints(a.acceleration.z);
+                // gyroscope
+                this->prints(g.gyro.x);
+                this->prints(g.gyro.y);
+                this->prints(g.gyro.z);
+                // temperature
+                this->prints(temp.temperature);
+            }else{
+                for (int i = 0; i<7; i++){
+                    this->prints(0);
+                }
+            }
+
+            // GUVA-S12SD
+            this->prints(guva.readIntensity());
 
             // CHECKSUM
             COMMS_SERIAL.print('*');
@@ -211,9 +251,12 @@ class Telemetry{
             this->send("Telemetry INIT: OK");
         }
 
-        void set_sleep_amount(){
-            sleepAmount = 1000 - (bitSize / commsBaudRate)*1000;
+        void set_sleep_amount(uint32_t elapsedTime){
+            uint32_t transmissionTime = (bitSize / commsBaudRate) * 1000;
+            elapsedTime -= transmissionTime;
+            sleepAmount = (1-percentActive)*10 * transmissionTime - elapsedTime;
         }
+
         String get_checksum(std::string data) {
             uint32_t checksum = CRC32::calculate(data.c_str(), data.length());
             char checksumStr[3];
