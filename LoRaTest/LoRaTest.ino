0742f6b023feb3c1bb10379d1b78f2e6ff093ced
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-12-30 21:41:25 +0100

    Add tester lora trasmission code

---
+++ LoRaTest/LoRaTest.ino
@@ -0,0 +1,22 @@
+#include <LoRa.h>
+#include <SPI.h>
+
+#define COMMS_SERIAL Serial
+
+void setup(){
+    COMMS_SERIAL.begin(9600);
+    while (!LoRa.begin(868E6)){
+        COMMS_SERIAL.println("Failed RF gateway");
+        delay(500);
+    }
+    LoRa.setSyncWord(12);
+}
+
+int counter;
+void loop(){
+    LoRa.beginPacket();
+    LoRa.print("Hello! ");
+    LoRa.print(counter++);
+    LoRa.endPacket();
+    delay(1000);
+}
