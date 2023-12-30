13990c52fd694b11de78b9eba87081de5a6d7189
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-12-30 21:53:58 +0100

    Remove Serial requirement from implementation of LoRa library

--- LoRaTest/LoRaTest.ino
+++ LoRaTest/LoRaTest.ino
@@ -1,12 +1,8 @@
 #include <LoRa.h>
 #include <SPI.h>
 
-#define COMMS_SERIAL Serial
-
 void setup(){
-    COMMS_SERIAL.begin(9600);
     while (!LoRa.begin(868E6)){
-        COMMS_SERIAL.println("Failed RF gateway");
         delay(500);
     }
     LoRa.setSyncWord(12); // set HTerm sync to the same!!
