22302b7f06685ec601f1507498243e98adeee84b
adamBacso <47544237+adamBacso@users.noreply.github.com>
2024-01-19 20:23:24 +0100

    restructure serial rf test

--- LoRaTestSerial/LoRaTestSerial.ino
+++ LoRaTestSerial/LoRaTestSerial.ino
@@ -1,6 +1,6 @@
-#define COMMS_SERIAL Serial1 
+#define COMMS_SERIAL    Serial1 
 
-#include <HardwareSerial.h>
+// #include <HardwareSerial.h>
 #include <SPI.h>
 
 
@@ -8,12 +8,16 @@ void setup(){
     COMMS_SERIAL.begin(115200);
     while (!COMMS_SERIAL){
     }
+    Serial.begin(9600);
 }
 
 int counter = 0;
 void loop(){
-    COMMS_SERIAL.print("sys get ver\r\n");
-    Serial.println(COMMS_SERIAL.read());
-    
-    delay(500);
+    if (COMMS_SERIAL.available()>0){
+        Serial.println(COMMS_SERIAL.read(),DEC);
+    }
+    else{
+        COMMS_SERIAL.println("sys get ver\r\n");
+        delay(500);
+    }
 }
