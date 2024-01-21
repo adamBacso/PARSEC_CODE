e90a7f6db5c08c274a4c16c68afc121a435439b5
adamBacso <47544237+adamBacso@users.noreply.github.com>
2024-01-21 18:50:53 +0100

    add testing data

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -48,6 +48,15 @@ class LightSensor {
 class MyGPS : public TinyGPSPlus {
     private:
         int gpsBaud;
+
+        const char *gpsStream =
+            "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+            "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+            "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+            "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+            "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+            "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+
     public:
         void begin(){
             this->SetBaud(9600);
@@ -59,9 +68,14 @@ class MyGPS : public TinyGPSPlus {
         }
 
         void FeedGPS(){
+            /*
             while(GPS_SERIAL.available()){
                 this->encode(GPS_SERIAL.read());
             }
+            */
+           while (*gpsStream){
+            this->encode(*gpsStream++);
+           }
         }
 };
 MyGPS gps;
