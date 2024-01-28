eae2477c49b981fd931a76c48d13c723c0b64662
adamBacso <47544237+adamBacso@users.noreply.github.com>
2024-01-28 13:11:30 +0100

    fixed mapping?

--- RemoteControl/RemoteControl.ino
+++ RemoteControl/RemoteControl.ino
@@ -1,7 +1,7 @@
 int currentAngle;
 int read_potentiometer(){
     int analogValue = analogRead(A0);
-    int angle = (analogValue/1024)*360;
+    int angle = map(analogValue, 0, 1023, -360, 360, 0);
     return angle;
 }
 
