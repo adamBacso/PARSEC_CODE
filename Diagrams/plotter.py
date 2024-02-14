9b0554baced270315d2899f537374f673ac5589e
adamBacso <47544237+adamBacso@users.noreply.github.com>
2024-02-14 15:00:38 +0100

    dictionary based diagrams and 3D plot

--- Diagrams/plotter.py
+++ Diagrams/plotter.py
@@ -1,129 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-packetID = []
-time = []
-internalTemperature = []
-gpsAge = []
-gpsLatitude = []
-gpsLongitude = []
-courseToTarget = []
-distanceToTarget = []
-currentCourse = []
-gpsAltitude = []
-barometricAltitude = []
-verticalSpeed = []
-bmeTemperature = []
-humidity = []
-accelerationX = []
-accelerationY = []
-accelerationZ = []
-gyroscopeX = []
-gyroscopeY = []
-gyroscopeZ = []
-mpuTemperature = []
+# Initialize a dictionary to hold all telemetry data
+telemetryData = {}
 
 telemetryPreamble = "radio_rx "
 
 def readFile(fileName):
-    f = open(fileName, "rt")
+    with open(fileName, "rt") as f:
+        # Read the first line to get keys (column names)
+        keys = f.readline().strip().split(",")
+        # Initialize lists for each key in the dictionary
+        for key in keys:
+            telemetryData[key] = []
 
-    for line in f:
+        for line in f:
+            if line.startswith(telemetryPreamble):
+                line = line[len(telemetryPreamble):].strip()
+                line = bytes.fromhex(line).decode('utf-8')
 
-        if line.startswith(telemetryPreamble):
-            line = line[len(line)]
-            line = bytes.fromhex(line).decode('utf-8')
+            lineArray = line.strip().split(",")
+            # Check if the lineArray length matches keys length to avoid index errors
+            if len(lineArray) == len(keys):
+                for key, value in zip(keys, lineArray):
+                    telemetryData[key].append(value)
 
-        lineArray = line.split(",")
-        index = 0
-        packetID.append(lineArray[index])
-        index += 1
-        time.append(lineArray[index])
-        index += 1
-        internalTemperature.append(lineArray[index])
-        index += 1
-        gpsAge.append(lineArray[index])
-        index += 1
-        gpsLatitude.append(lineArray[index])
-        index += 1
-        gpsLongitude.append(lineArray[index])
-        index += 1
-        courseToTarget.append(lineArray[index])
-        index += 1
-        distanceToTarget.append(lineArray[index])
-        index += 1
-        currentCourse.append(lineArray[index])
-        index += 1
-        gpsAltitude.append(lineArray[index])
-        index += 1
-        barometricAltitude.append(lineArray[index])
-        index += 1
-        verticalSpeed.append(lineArray[index])
-        index += 1
-        bmeTemperature.append(lineArray[index])
-        index += 1
-        humidity.append(lineArray[index])
-        index += 1
-        accelerationX.append(lineArray[index])
-        index += 1
-        accelerationY.append(lineArray[index])
-        index += 1
-        accelerationZ.append(lineArray[index])
-        index += 1
-        gyroscopeX.append(lineArray[index])
-        index += 1
-        gyroscopeY.append(lineArray[index])
-        index += 1
-        gyroscopeZ.append(lineArray[index])
-        index += 1
-        mpuTemperature.append(lineArray[index])
-        index += 1
-    f.close()
+def draw_plot(yLabel, xLabel = 'missionTime', formatting = "o-", label = ""):
+    y = telemetryData[yLabel]
+    x = telemetryData[xLabel]
+    if not x:
+        x = list(range(len(y)))
 
-def draw_plot(y, x = [], formatting = "o-", label = ""):
-    if x == []:
-        for i in range(len(y)):
-            x.append(i)
+    # Convert x and y to floats, discarding unconvertible values
+    x, y = zip(*[(float(xi), float(yi)) for xi, yi in zip(x, y) if xi.replace('.','',1).isdigit() and yi.replace('.','',1).isdigit()])
 
-    if len(x) == len(y):
-        i = 0
-        while i<len(y):
-            try:
-                y[i] = float(y[i])
-                i+=1
-            except ValueError:
-                del y[i]
-                del x[i]
-        
-        i = 0
-        while i<len(x):
-            try:
-                x[i] = float(x[i])
-                i+=1
-            except ValueError:
-                del y[i]
-                del x[i]
-
-        plt.plot(x,y,formatting, label=label, markersize=3)
+    plt.plot(x, y, formatting, label=label, markersize=3)
 
 def show_plot(title = ""):
     plt.legend()
     plt.title(title)
     plt.show()
+
+def spacial_plot(xData='latitude', yData='longitude', zData='barometricAltitude', xLabel='Latitude', yLabel="Longitude", zLabel='Altitude (BME)'):
+    x = telemetryData[xData]
+    y = telemetryData[yData]
+    z = telemetryData[zData]
+
+    fig = plt.figure()
+    splot = fig.add_subplot(projection='3d')
+    x, y, z = zip(*[(float(xi), float(yi), float(zi)) for xi, yi, zi in zip(x, y, z) if xi.replace('.', '', 1).isdigit() and yi.replace('.', '', 1).isdigit() and zi.replace('.', '', 1).isdigit()])
+
+    splot.scatter(x,y,z,marker="o")
+
+    splot.set_xlabel(xLabel)
+    splot.set_ylabel(yLabel)
+    splot.set_zlabel(zLabel)
+
+    plt.show()
+
+# Example usage
 readFile("Logging/on-board data/flight3.txt")
 
-draw_plot(barometricAltitude,time,label="Altitude (BME)")
+draw_plot('barometricAltitude', label="Altitude (BME)")
 show_plot("Altitude - time")
 
-draw_plot(verticalSpeed,time,label="Vertical Speed")
+draw_plot('verticalSpeed', label="Vertical Speed")
 show_plot("VSpeed - time")
 
-draw_plot(accelerationX,time, label="Acceleration X")
-draw_plot(accelerationY,time, label="Acceleration Y")
-draw_plot(accelerationZ,time, label="Acceleration Z")
+draw_plot('accelerationX', label="Acceleration X")
+draw_plot('accelerationY', label="Acceleration Y")
+draw_plot('accelerationZ', label="Acceleration Z")
 show_plot("Acceleration - time graph")
 
-draw_plot(gyroscopeX,time, label="Rotation X")
-draw_plot(gyroscopeY,time, label="Rotation Y")
-draw_plot(gyroscopeZ,time, label="Rotation Z")
+draw_plot('gyroscopeX', label="Rotation X")
+draw_plot('gyroscopeY', label="Rotation Y")
+draw_plot('gyroscopeZ', label="Rotation Z")
 show_plot("Rotation - time graph")
+
+spacial_plot()
