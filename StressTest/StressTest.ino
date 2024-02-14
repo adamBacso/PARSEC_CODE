dbf4de4d2823e6436863b9e7961e83dc9797662a
adamBacso <47544237+adamBacso@users.noreply.github.com>
2024-02-14 17:56:09 +0100

    added serial monitoring

--- StressTest/StressTest.ino
+++ StressTest/StressTest.ino
@@ -9,9 +9,10 @@ Adafruit_SGP40 sgp;
 
 void setup(){
     Serial2.begin(115200);
+    Serial.begin(9600);
     bme.begin(0x76);
     mpu.begin(0x68);
-    sgp.begin(&Wire);
+    sgp.begin(&Wire1);
     sgp.selfTest();
 }
 
@@ -19,6 +20,7 @@ void loop(){
     float temperature = bme.readTemperature();
     float humidity = bme.readHumidity();
     float pressure = bme.readPressure();
+    float vocIndex = sgp.measureVocIndex(temperature,humidity);
 
     sensors_event_t a,g,t;
     mpu.getEvent(&a,&g,&t);
@@ -32,6 +34,7 @@ void loop(){
 
     
     String concatenatedData = String(temperature, HEX) + "2c" + String(humidity, HEX) + "2c" + String(pressure, HEX) + "2c" + String(ax, HEX) + "2c" + String(ay, HEX) + "2c" + String(az, HEX) + "2c" + String(gx, HEX) + "2c" + String(gy, HEX) + "2c" + String(gz, HEX) + "2c" + String(temp, HEX);
-    Serial2.println("radio tx "+concatenatedData+" 1");
+    String plainData = String(temperature) + "," + String(humidity) + "," + String(pressure) + "," + String(ax) + "," + String(ay) + "," + String(az) + "," + String(gx) + "," + String(gy) + "," + String(gz) + "," + String(temp) + "," + String(vocIndex);
+    Serial.println("radio tx "+plainData+" 1");
     delay(250);
 }
